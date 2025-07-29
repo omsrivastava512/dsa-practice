acc808f773d57dbdf1c8326d682f973596a136e4
omsrivastava512 <79087911+omsrivastava512@users.noreply.github.com>
2025-07-30 04:19:00 +0530

    Solve Linked Stack - 1

--- C++/src/stack/LinkedStack.cpp
+++ C++/src/stack/LinkedStack.cpp
@@ -1,28 +1,75 @@
+#include <iostream>
+using namespace std;
 // Node for stack
-struct StackNode {
+struct Node {
     int data;
-    StackNode* next;
-    
-    StackNode(int val) : data(val), next(nullptr) {}
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+    Node(int val, Node*next) : data(val), next(next) {}
 };
 
 class LinkedStack {
 private:
-    StackNode* top;
+    Node* top;
     int count;
+
     
 public:
-    LinkedStack();
-    ~LinkedStack();
+    LinkedStack(): top(nullptr), count(0){}
+    ~LinkedStack(){
+        clear();
+    }
     
     // Basic operations:
-    void push(int data);
-    int pop();
-    int peek();
-    bool isEmpty();
-    int size();
-    void display();
-    void clear();
+    void push(int data){
+        if(!top){
+            top = new Node(data);
+        }
+        else {
+            top = new Node(data, top);
+        }
+        count++;
+    }
+    int pop(){
+        if(!top){
+            cout<<"Underflow";
+            return INT16_MIN;
+        }
+        int data = top->data;
+        Node * currentTop = top;
+        top = top->next;
+        delete currentTop;
+        count--;
+        return data;
+    }
+    int peek()const{
+        if(!top){
+            cout<<"Underflow";
+            return INT16_MIN;
+        }
+        return top->data;
+    }
+    bool isEmpty()const{ return new bool(top);}
+    int size()const {return count;}
+
+    void display()const{
+        if(!top){
+            cout<<"Stack Empty"<<endl;
+            return;
+        }
+        Node*curr = top;
+        while (curr)
+        {
+            cout<<curr->data<<" ";
+        }
+        cout<<endl;
+    }
+    void clear(){
+        while (!isEmpty())
+        {
+            pop();
+        }
+    }
     
     // Advanced operations:
     void reverse();
