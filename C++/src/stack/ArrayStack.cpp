47c0a8d2e090009b73fc2b36f527b448b5d28d0c
omsrivastava512 <79087911+omsrivastava512@users.noreply.github.com>
2025-07-30 19:13:00 +0530

    Solve ArrayStack

--- C++/src/stack/ArrayStack.cpp
+++ C++/src/stack/ArrayStack.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
 class ArrayStack {
 private:
     int* arr;
@@ -5,18 +9,50 @@ private:
     int capacity;
     
 public:
-    ArrayStack(int size);
-    ~ArrayStack();
+    ArrayStack(int cap): arr(new int[cap]), capacity(cap), top(-1){}
+    ~ArrayStack(){
+        delete[] arr;
+    }
     
     // Basic operations to implement:
-    void push(int data);
-    int pop();
-    int peek();
-    bool isEmpty();
-    bool isFull();
-    int size();
-    void display();
-    void clear();
+    void push(int data){
+        if(isFull()){
+            cout<<"Overflow!"<<endl;
+            return;
+        }
+        arr[++top] = data;
+    }
+    int pop(){
+        if(isEmpty()){
+            cout<<"Underflow"<<endl;
+            return INT16_MIN;
+        }
+        return arr[top--];
+    }
+    int peek()const{
+        if(isEmpty()){
+            cout<<"Underflow"<<endl;
+            return INT16_MIN;
+        }
+        return arr[top];
+    }
+    bool isEmpty()const{ return top==-1;}
+    bool isFull()const{ return top==capacity-1; }
+    int size()const{return top+1;}
+
+    void display()const{
+        if(isEmpty()){
+            cout<<"Underflow"<<endl;
+            return;
+        }
+        for(int i = 0; i<=top; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    void clear(){
+        top = -1;
+    }
     
     // Advanced operations:
     int getMin(); // Get minimum element in O(1)
@@ -38,3 +74,10 @@ public:
     int getMin();
     bool empty();
 };
+
+
+
+// Learnings: delete[] releases the array allocated using new[] such as new int[]
+// delete[] arr; // ✅ tells C++: call element destructors, then free entire block
+// delete arr;   // ❌ wrong for arrays: only first element is destroyed, UB
+// delete arr[]; // ❌ invalid syntax: compiler error
